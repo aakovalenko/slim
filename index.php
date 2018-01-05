e2c8fcf0164a2136cc0bf755b1949eec43dd9208
kaaSLDN <kaa89pl@gmail.com>
2018-01-05 17:05:15 +0200

    install slim/twig

--- index.php
+++ index.php
@@ -25,8 +25,21 @@ $app->get('/', function(){
     echo 'users';
 });*/
 
-$app->get('/', function (){
-    echo $this->nothing;
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'views', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
+$app->get('/', function () {
+
 });
 
 $app->run();
