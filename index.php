d97e168cae8cc41abf08bd019bf0c12635042050
kaaSLDN <kaa89pl@gmail.com>
2018-01-05 16:47:35 +0200

    displayErrorDetails

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App();
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
 
 $container = $app->getContainer();
 
-$container['greeting'] = function () {
+/*$container['greeting'] = function () {
     echo 'abc';
     return 'Hello from the container!';
 };
@@ -15,10 +19,14 @@ $app->get('/', function(){
     echo $this->greeting.PHP_EOL;
     echo $this->greeting.PHP_EOL;
     echo $this->greeting.PHP_EOL;
-});
+});*/
 
-$app->get('/users', function(){
+/*$app->get('/users', function(){
     echo 'users';
+});*/
+
+$app->get('/', function (){
+    echo $this->nothing;
 });
 
 $app->run();
